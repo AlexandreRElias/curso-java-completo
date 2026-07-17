248f1b6efa3397d80ab3bd20a3de10f1ad054f4a
Alexandre Elias <alexandre_elias_dev@outlook.com.br>
2026-07-17 15:36:16 -0300

    Adicionado exercícios List

---
+++ JavaTrainingList/src/application/Register.java
@@ -0,0 +1,84 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entities.Employee;
+
+public class Register {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee employee;
+		List<Employee> listEmployee = new ArrayList<>();
+		
+		System.out.print("How many employers will be registered? ");
+		Integer employeeNumber = sc.nextInt();
+		sc.nextLine();
+		
+		for (int i = 0; i < employeeNumber; i++) {
+			
+			System.out.println("Employee #" + (i+1) + ":");
+			
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			sc.nextLine();
+			
+			System.out.print("Nome: ");
+			String name = sc.nextLine();
+			
+			System.out.print("Salário: ");
+			double salary = sc.nextDouble();
+			sc.nextLine();
+			
+			
+			
+			employee = new Employee(id, name, salary);
+			
+			listEmployee.add(i, employee);	
+		}
+		
+		System.out.println();
+		System.out.print("Enter the employer id that will have salary increase: ");
+		int idIncreaseSalary = sc.nextInt();
+		sc.nextLine();
+		
+		Employee emp = listEmployee.stream().filter(x -> x.getId() == idIncreaseSalary).findFirst().orElse(null);
+		
+		//Integer pos = position(listEmployee, idIncreaseSalary);
+		
+		//if (pos == null) {
+		if(emp == null) {	
+			System.out.println("This id does not exist!");
+		}
+		else {
+			System.out.print("Enter the percentage: ");
+			double percentIncreaseSalary = sc.nextDouble();		
+			//listEmployee.get(pos).increaseSalary(percentIncreaseSalary);
+		}	
+		System.out.println();
+		System.out.println("List of employees:");
+		for(Employee employe : listEmployee) {
+			System.out.println(emp);
+		}
+		
+		sc.close();
+	}
+	
+	public static Integer position(List<Employee> list, int id) {
+		
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == id) {
+				return i;
+			}
+		}
+		return null;
+	}
+
+}
